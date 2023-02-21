9aad572204ff7b8c366c54ae8bd0f21d603eaff1
3rick <erick.dejesus1@alumno.buap.mx>
2023-02-21 03:18:39 -0600

    focus to first placeholder

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputs = document.querySelectorAll("input"),
+button = document.querySelector("button");
+// console.log(inputs, button)
+
+window.addEventListener("load", () => inputs[0].focus())
